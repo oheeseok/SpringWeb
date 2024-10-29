7b9e5bf38de6fc7c38a34071434fc56f14204484
oheeseok <heeseok96@gmail.com>
2024-10-29 09:36:42 +0900

    JUnit Test

---
+++ src/test/java/org/example/springweb/service/PostServiceTest.java
@@ -0,0 +1,35 @@
+package org.example.springweb.service;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+  @BeforeAll
+  static void init() {
+    System.out.println("init");
+  }
+
+  @BeforeEach
+  void setUp() {
+    System.out.println("메소드 실행 전");
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("메소드 실행 후");
+  }
+  
+  @Test
+  void getAllPosts() {
+    System.out.println("모든 게시판 글 목록 조회");
+  }
+
+  @Test
+  void getPostDetail() {
+    System.out.println("게시판 글 조회");
+  }
+}
